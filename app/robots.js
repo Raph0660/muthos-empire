ac6ef71c96a08b9565020482c83dabbd3317fa41
Raph0660 <raphael.geronimi1@gmail.com>
2026-05-13 11:51:43 +0200

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.muthos-empire.com/sitemap.xml',
+    sitemap: 'https://www.ideescasa.fr/sitemap.xml',
   }
 }
