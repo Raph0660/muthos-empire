45142f46f5b407704ad5178f87e74705c49da023
Raph0660 <raphael.geronimi1@gmail.com>
2026-05-13 12:35:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,21 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        // Capture toutes les URLs de catégories (deco, jardin, immo, etc.)
         source: '/category/:path*',
         destination: '/',
         permanent: true,
       },
       {
-        // Capture toutes les URLs d'archives par date (2025/02/...)
         source: '/2025/:path*',
         destination: '/',
         permanent: true,
       },
       {
-        // Capture tous les anciens fichiers .php
         source: '/:slug*.php',
         destination: '/',
         permanent: true,
       },
       {
-        // Capture le blog et les auteurs
         source: '/blog/:path*',
         destination: '/',
         permanent: true,
@@ -33,14 +29,12 @@ const nextConfig = {
       }
     ];
   },
-images: {
+  images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.boulanger.com' },
       { protocol: 'https', hostname: '**.coffee-webstore.com' },
-      { protocol: 'https', hostname: '**.maxicoffee.com' }, 
-      { protocol: 'https', hostname: '**.scene7.com' },
-    ],
-  },
+      { protocol: 'https', hostname: '**.maxicoffee.com' },
+      { protocol: 'https', hostname: '**.scene7.com' }
     ],
   },
 };
